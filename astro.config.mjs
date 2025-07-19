3c7e43122e1027178c145603cc168a63fb210771
KrishDemandFlow <krish@demandflow.co>
2025-07-19 20:24:10 +0530

    cloudflare configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-domain.com', // Replace with your actual domain
+  site: 'https://espressopicker.com',
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: cloudflare(),
   integrations: [svelte()],
 
   vite: {
